23af667f51240c30081816d8f6b31cc0347a3713
Rodrigo Soares <adsrodsoares@gmail.com>
2021-01-10 22:38:20 -0300

    Set order location

--- src/Orders/index.tsx
+++ src/Orders/index.tsx
@@ -1,10 +1,28 @@
+import { useEffect, useState } from 'react';
 import './styles.css';
 import StepsHeader from './StepsHeader';
+import ProductList from './ProductsList';
+import { OrderLocationData, Product } from './types';
+import { fetchProducts } from './api';
+import OrderLocation from './OrderLocation';
+
 
 function Orders() {
+    const [products, setProducts] = useState<Product[]>([]);
+    const [orderLocation, setOrderLocation] = useState<OrderLocationData>();
+    
+    useEffect(() => {
+        fetchProducts()
+        .then(response => setProducts(response.data))
+        .catch(error => console.log(error))
+
+    }, []);
+    
     return (
         <div className="orders-container">
             <StepsHeader />
+            <ProductList products={products} />            
+            <OrderLocation onChangeLocation={location => setOrderLocation(location) }/>
         </div>
     )
 }
---
+++ src/Orders/types.ts
@@ -0,0 +1,16 @@
+import OrderLocation from './OrderLocation';
+
+export type Product = {    
+    
+    id: number;
+    name: string;
+    price: number;
+    description: string;
+    imageUri: string;
+}
+
+export type OrderLocationData = {
+    latitude: number;
+    longitude: number;
+    address: string;
+}
